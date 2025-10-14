f754068ced702c965a5ba0ab1c4a3138e323da6d
JiaXuan-coder <jqua0016@student.monash.edu>
2025-10-14 14:10:14 +0800

    Update index.ts supabase URL

--- supabase/functions/classrooms/index.ts
+++ supabase/functions/classrooms/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = Deno.env.get('SUPABASE_URL') ?? ''
+const supabaseUrl = "https://ttisvmwrxnfbedrboizq.supabase.co"
 const supabaseKey = Deno.env.get('SUPABASE_ANON_KEY') ?? ''
 const serviceRoleKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
 
@@ -526,3 +526,4 @@ async function deleteClassroom(supabaseClient: any, classroomId: string) {
     )
   }
 }
+
